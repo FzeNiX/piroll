755da97b7d883dfb14a7a271cb354fd0442fe34e
FzeNiX <zfzenix@gmail.com>
2020-11-19 14:59:11 +0700

    Upload files via upload

--- img/v1LateFPT.py
+++ img/v1LateFPT.py
@@ -9,7 +9,6 @@ requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-S
 AUTHORIZATION_KEY = "Basic dGh1eWhrMkBmcHQuY29tLnZuOjEyMzQ1Ng=="
 APP_AUTHORIZATION_KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIxMjQ3IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6Iktob2FOVjE3QGZwdC5jb20udm4iLCJBc3BOZXQuSWRlbnRpdHkuU2VjdXJpdHlTdGFtcCI6IlVJTklLSEg3M0ZJTUxPNUU2T0M0UVdTT0daRkpDTUM3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiRW1wbG95ZWUiLCJodHRwOi8vd3d3LmFzcG5ldGJvaWxlcnBsYXRlLmNvbS9pZGVudGl0eS9jbGFpbXMvdGVuYW50SWQiOiIxIiwiRW1wbG95ZWVJZENsYWltIjoiMjEyMjIiLCJzdWIiOiIyMTI0NyIsImp0aSI6IjdkZTJkM2U5LWFiYTItNGQxNC04NTgxLWU5YmE1YmViNDEzMiIsImlhdCI6MTYwNDQ2NjY4NCwibmJmIjoxNjA0NDY2Njg0LCJleHAiOjE2MTIyNDI2ODQsImlzcyI6IkhSSVMiLCJhdWQiOiJIUklTIn0.N8HQCtkSKj6YZWUJxIkvyI6ixn3NcOF99Ei4FC97ByA"
 
-ALERT = 0
 def pushInfo(title, messages):
     bot = "bot1263316426"  
     telegramAPI = "https://api.telegram.org/"+ bot +":AAEsMDr5IUbrwRVV2jGgudIwrAuQNiPpy_Q/sendMessage?chat_id=540921490&text=" + title + ":\n" + messages
@@ -147,27 +146,6 @@ def vuCheck(checkType):
         pushInfo("\U0001F629 Request type "+ str(checkType), "fail!, exit program...")
         exit()
 
-def getShopeeCoin():
-    url = "https://shopee.vn:443/mkt/coins/api/v2/checkin"
-    cookies = {"SPC_F": "351564216461632_fab1e307c22cc697_307c22cc697fab1e", "UA": "Shopee%20Android%20Beeshop%20locale%2Fvi%20version%3D413%20appver%3D26210", "SPC_AFTID": "114c5172-b8a3-405f-8289-c21ce735daa1", "shopee_app_version": "26210", "csrftoken": "r7y1Gvhw5xfoOw1ErBtbgmnQ6nkYOtiv", "SPC_CLIENTID": "DYaxnW9OavNiSBrtmgutsgogqjegbwqa", "REC_T_ID": "3926955b-2889-11eb-b707-10c172968115", "SPC_F": "351564216461632_fab1e307c22cc697_307c22cc697fab1e", "SPC_SI": "bffvn.nxdAlqbkNGsDd9rXmqq2Rj4CaTHhB7vo", "shopee_rn_version": "1605150888", "SPC_RNBV": "4037010", "userid": "18714056", "shopee_token": "l2V2S9T4+5GyfKwsx0Jgex4ov7HGRl9gmkkB8rCVqk0=", "username": "fzenix", "SPC_U": "18714056", "REC_MD_45": "1605586227", "REC_MD_41_1000121": "1605585928_0_50_0_44", "SPC_EC": "NLK6nTjccFxbee143RxE/j4NW//cY0oocEtFKl8ftcTo+TH8bdg9QfGzBiJTZYK+lvM4rYHDO89urGf3jH3btuO1PuQFexqg25Yiu5YXARa6mIveT1FIw4nGnvSB8pxJ87cFZpANuKCF5H7BDKCedg==", "SPC_R_T_ID": "qhO0SoR7gmB2mDEc/+i2vjaB+6pwAtU9oqfp5J7mZwMTWRavo7mWVCLUIEP3OzSfbDddQCXGbpp3tHhYLluXrvkfjh9HLC9PY4uA7haEKDA=", "SPC_R_T_IV": "dnSFo8KQnV4t7U6NuDy0+Q==", "_ga": "GA1.2.722922578.1605585657", "_gid": "GA1.2.1436838490.1605585657", "_gat": "1"}
-    headers = {"x-csrftoken": "r7y1Gvhw5xfoOw1ErBtbgmnQ6nkYOtiv", "x-api-source": "rn", "x-shopee-language": "vi", "referer": "https://shopee.vn/mkt/coins/api//bridge_cmd?cmd=reactPath%3Ftab%3Dbuy%26path%3Dshopee%252FHOME_PAGE%253Fis_tab%253Dtrue", "accept": "application/json", "content-type": "application/json", "if-none-match-": "55b03-afb69e438c4b48e93e5e3a9f7e6a9111", "Connection": "close", "Accept-Encoding": "gzip, deflate", "User-Agent": "Android app Shopee appver=26210 app_type=1"}
-    resp = requests.post(url, headers=headers, cookies=cookies)
-    data = json.loads(resp.text)
-    try:
-        msg = data['msg']
-        jData = data['data']
-        check_in_day = jData['check_in_day']
-        increase_coins = jData['increase_coins']
-        if(msg == "success"):
-            message = "Got " + str(increase_coins) + " coins today! " + "\nStreak: " + str(check_in_day) + " day(s)"
-            pushInfo("\U0001F514 Shopee Coins", message)
-        else:
-            message = "Something Wrong!"
-            pushInfo("\U0001F514 Shopee Coins", message)
-    except:
-        message = "Shopee cookie may expired"
-        pushInfo("\U000026A0 Shopee Coins", message)
-
 dateTime = getCurrentTime()
 day = dateTime.strftime("%a")
 
@@ -216,9 +194,5 @@ if (day != "Sat" and day != "Sun"):
         pushInfo("\U0001F4A1 Info", "Nothing to do now, gotta work \U0001F4AA")
 else:
     pushInfo("\U0001F4A1 Info", "Enjoy The Weekend! \U0001F604")
-
-#Another Job. LOL
-if (dateTime.hour < 10):
-    getShopeeCoin()
 
 
